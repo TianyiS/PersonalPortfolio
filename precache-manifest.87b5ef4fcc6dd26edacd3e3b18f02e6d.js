b99f5b57cedc399e768115767a1e3b30220aaf5c
TianyiS <sangtianyi01@gmail.com>
2018-11-23 20:52:37 -0800

    Updates

---
+++ precache-manifest.87b5ef4fcc6dd26edacd3e3b18f02e6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c493b97133745b5ab90",
+    "url": "/PersonalPortfolio/static/js/runtime~main.3c493b97.js"
+  },
+  {
+    "revision": "8204613d531f5bc3704d",
+    "url": "/PersonalPortfolio/static/js/main.8204613d.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/PersonalPortfolio/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "8204613d531f5bc3704d",
+    "url": "/PersonalPortfolio/static/css/main.7122c26b.chunk.css"
+  },
+  {
+    "revision": "efa81f0d860c1ef38baaeb57a744ccf9",
+    "url": "/PersonalPortfolio/index.html"
+  }
+];
